7ae54430184a33c0aa3d945f193ca4712e3c457d
Monisola Akeredolu <80970391+Monnie-ruth@users.noreply.github.com>
2025-02-28 08:06:40 +0100

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,11 @@
-
+number1 = 10
+number2 = 5
+print("Addition of numbers")
+result_1 = (number1 + number2)
+print(result_1)
+print("Difference of numbers")
+result_2 = (number2 - number1)
+print(result_2)
+print("Product of numbers")
+result_3 = (number1 * number2)
+print(result_3)
